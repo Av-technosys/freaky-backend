3da2f68b55a0bb896f6aa44d782abd706266ad04
nishanka1304 <nishank@lazyatra.com>
2025-11-27 13:27:48 +0530

    fix user address apis

--- src/routes/v1/user.route.js
+++ src/routes/v1/user.route.js
@@ -6,6 +6,7 @@ import {
   listAllAddresses,
   setCurrentAddress,
   updateUserInfo,
+  deleteAddress
 } from "../../controllers/User.controllers.js";
 import { confirmUserToken } from "../../middleware/user.middleware.js";
 
@@ -16,7 +17,8 @@ router.get("/list_all_addresses", confirmUserToken, listAllAddresses);
 
 router.post("/update_personal_info", confirmUserToken, updateUserInfo);
 router.post("/add_address", confirmUserToken, addAddress);
-router.post("/edit_address", confirmUserToken, editAddresses);
+router.post("/edit_address/:id", confirmUserToken, editAddresses);
 router.post("/set_current_address", confirmUserToken, setCurrentAddress);
+router.post("/delete_address/:id", confirmUserToken, deleteAddress);
 
 export default router;

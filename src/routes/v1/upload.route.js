96210c734d81828039008540b8bef463534a7d73
ravindrasingh777 <ravindrasinghrss2004@gmail.com>
2026-01-02 15:28:11 +0530

    all api's done for managing event types

--- src/routes/v1/upload.route.js
+++ src/routes/v1/upload.route.js
@@ -1,11 +1,11 @@
-import { Router } from "express";
-import {uploadUrl} from "../../controllers/Upload.controllers.js";
+import { Router } from 'express';
+import { uploadUrl } from '../../controllers/Upload.controllers.js';
 
-import { confirmUserToken } from "../../middleware/user.middleware.js";
+import { confirmUserToken } from '../../middleware/user.middleware.js';
 
 const router = Router();
 
-router.post("/get_S3_url", confirmUserToken, uploadUrl);
-
+// router.post("/get_S3_url", confirmUserToken, uploadUrl);
+router.post('/get_S3_url', uploadUrl);
 
 export default router;
